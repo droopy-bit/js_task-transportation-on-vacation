f992cb4360fbc47a7bd415928c2574c03f557ccd
Yurii Kondrashenko <droopy215421@gmail.com>
2026-03-09 12:36:51 +0100

    completed tasks

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,31 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  // write code here
+const WEEK_RENTAL_DAYS = 7;
+const WEEK_RENTAL_PRICE = 230;
+
+const SIX_DAY_RENTAL_DAYS = 6;
+const SIX_DAY_RENTAL_PRICE = 220;
+
+const THREE_DAY_RENTAL_DAYS = 3;
+const THREE_DAY_RENTAL_PRICE = 100;
+
+const DEFAULT_RENTAL_PRICE = 80;
+
+function calculateRentalCost(numberOfDays) {
+  if (numberOfDays === WEEK_RENTAL_DAYS) {
+    return WEEK_RENTAL_PRICE;
+  }
+
+  if (numberOfDays === SIX_DAY_RENTAL_DAYS) {
+    return SIX_DAY_RENTAL_PRICE;
+  }
+
+  if (numberOfDays === THREE_DAY_RENTAL_DAYS) {
+    return THREE_DAY_RENTAL_PRICE;
+  }
+
+  return DEFAULT_RENTAL_PRICE;
 }
 
 module.exports = calculateRentalCost;
